8c4d8b2b7deb98405ebce95c79d2c6b7e021cd1f
Anastasia Mitrofanova <mitrofanovaa70@gmail.com>
2023-02-18 17:43:05 +0300

    Функции

---
+++ js/functions.js
@@ -0,0 +1,39 @@
+function findStringLength(stringValue, minLength) {
+  return String(stringValue).length >= minLength;
+}
+
+findStringLength('house', 5);
+
+function isPalindrome(stringValue) {
+  stringValue = stringValue.toLowerCase();
+  stringValue = stringValue.replaceAll(' ', '');
+  return stringValue === stringValue.split('').reverse().join('');//превращаем в массив, перевертываем и превращаем обратно в строку
+}
+
+isPalindrome('Лёша на полке клопа нашёл ');
+
+function findNumber(stringValue) {
+  let onlyNumbers = '';
+  for(const char of stringValue) {
+    if(!isNaN(parseInt(char, 10))) {
+      onlyNumbers += char;
+    }
+  }return parseInt(onlyNumbers, 10);
+}
+
+findNumber('-2023 year 0.3');
+findNumber('а я томат');
+
+function createString(stringValue, minLength, symbols) {
+  while(stringValue.length < minLength) {
+    if(symbols.length <= minLength - stringValue) {
+      stringValue = symbols + stringValue;
+    }else {
+      const cutSymbols = minLength - stringValue.length;
+      stringValue = symbols.slice(0, cutSymbols) + stringValue;
+    }
+  }
+  return stringValue;
+}
+
+createString('q', 4, 'werty');
