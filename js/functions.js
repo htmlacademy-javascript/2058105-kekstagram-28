b7d089ac48be3fadc0fa9fa31f69ca92c165ef3e
Anastasia Mitrofanova <mitrofanovaa70@gmail.com>
2023-02-20 14:54:15 +0300

    Упрощает и переделывает функции на стрелочные

--- js/functions.js
+++ js/functions.js
@@ -1,39 +1,31 @@
-function findStringLength(stringValue, minLength) {
-  return String(stringValue).length >= minLength;
-}
+const findStringLength = (string, minLength) => String(string).length >= minLength;
 
 findStringLength('house', 5);
 
-function isPalindrome(stringValue) {
-  stringValue = stringValue.toLowerCase();
-  stringValue = stringValue.replaceAll(' ', '');
-  return stringValue === stringValue.split('').reverse().join('');
-}
+const isPalindrome = (string) => {
+  string = String(string).toLowerCase().replaceAll(' ', '');
+  return string === string.split('').reverse().join('');
+};
 
 isPalindrome('Лёша на полке клопа нашёл ');
 
-function findNumber(stringValue) {
-  let onlyNumbers = '';
-  for(const char of stringValue) {
-    if(!isNaN(parseInt(char, 10))) {
-      onlyNumbers += char;
-    }
-  }return parseInt(onlyNumbers, 10);
-}
+const findNumber = (string) => {
+  const onlyNumbers = String(string).replace(/\D/g, '');
+  return parseInt(onlyNumbers, 10);
+};
 
 findNumber('-2023 year 0.3');
 findNumber('а я томат');
 
-function createString(stringValue, minLength, symbols) {
-  while(stringValue.length < minLength) {
-    if(symbols.length <= minLength - stringValue) {
-      stringValue = symbols + stringValue;
-    }else {
-      const cutSymbols = minLength - stringValue.length;
-      stringValue = symbols.slice(0, cutSymbols) + stringValue;
-    }
+const createString = (string, length, symbols) => {
+  if(string.length >= length) {
+    return string;
   }
-  return stringValue;
-}
+  while(string.length < length) {
+    const cutSymbols = length - string.length;
+    string = symbols.slice(0, cutSymbols) + string;
+  }
+  return string;
+};
 
-createString('q', 4, 'werty');
+createString('house', 10, 's');
