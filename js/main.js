e16c1daccb26fc7062f716cd7992698c2d795d12
Anastasia Mitrofanova <mitrofanovaa70@gmail.com>
2023-02-24 19:27:16 +0300

    Исправляет массив с комментариями

--- js/main.js
+++ js/main.js
@@ -12,7 +12,8 @@ const DESCRIPTIONS = [
   'Подпишитесь на меня, поставьте лайки',
   'Люблю постить всякую ерунду',
   'Хочу поехать серфить в Африку',
-  'Я бомж'
+  'Я бомж',
+  'Сегодня отличная погода'
 ];
 
 const MESSAGES = [
@@ -24,54 +25,39 @@ const MESSAGES = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?'
 ];
 
-const getRandomInteger = (min, max) => {
-  const lower = Math.ceil(Math.min(min, max));
-  const upper = Math.floor(Math.max(min, max));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
+const PHOTOS_COUNT = 25;
+let photoId = 1;
+let commentId = 1;
 
-const getUniqId = (min, max) => {
-  const previousId = [];
-  return function () {
-    let currentId = getRandomInteger(min, max);
-    if (previousId.length >= (max - min + 1)) {
-      return null;
-    }
-    while (previousId.includes(currentId)) {
-      currentId = getRandomInteger(min, max);
-    }
-    previousId.push(currentId);
-    return currentId;
-  };
-};
-
-const photoId = getUniqId(1, 25);
-const urlPhotoId = getUniqId(1, 25);
-const commentId = getUniqId(1, 25);
-
+const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
-const SIMILAR_PHOTOS_COUNT = 25;
+
+const createMessage = () => {
+  const messages = Array.from({length: getRandomInteger(1, 2)}, () => getRandomArrayElement(MESSAGES));
+  return [...new Set(messages)].join(' ');
+};
 
 const createComment = () => {
   const comment = {
-    id: commentId(),
+    id: commentId,
     avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
-    message: getRandomArrayElement(MESSAGES),
+    message: createMessage(),
     name: getRandomArrayElement(NAMES),
-  }; return comment;
+  };
+  commentId += 1;
+  return comment;
 };
 
 const createPhoto = () => {
   const photo = {
-    id: photoId(),
-    url: `photos/${urlPhotoId()}.jpg`,
+    id: photoId,
+    url: `photos/${photoId}.jpg`,
     descripton: getRandomArrayElement(DESCRIPTIONS),
     likes: getRandomInteger(15, 200),
-    comments: createComment()
+    comments: Array.from({length: getRandomInteger(1, 6)}, createComment)
   };
+  photoId += 1;
   return photo;
 };
 
-const similarPhotos = Array.from({length: SIMILAR_PHOTOS_COUNT}, createPhoto);
-console.log(similarPhotos);
+const createPhotos = () => Array.from({length: PHOTOS_COUNT}, createPhoto);
