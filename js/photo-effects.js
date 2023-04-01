b76d5190c8fa41b47ad23e51ac11a05a9a86f916
Anastasia Mitrofanova <mitrofanovaa70@gmail.com>
2023-04-01 17:34:30 +0300

    Данные для фото-эффектов

---
+++ js/photo-effects.js
@@ -0,0 +1,32 @@
+const PHOTO_EFFECTS = {
+  chrome: {
+    name: 'grayscale',
+    min: 0,
+    max: 1,
+    step: 0.1
+  },
+  sepia: {
+    name: 'sepia',
+    min: 0,
+    max: 1,
+    step: 0.1
+  },
+  marvin: {
+    name: 'invert',
+    min: 0,
+    max: 100,
+    step: 1
+  },
+  phobos: {
+    name: 'blur',
+    min: 0,
+    max: 3,
+    step: 0.1
+  },
+  heat: {
+    name: 'brightness',
+    min: 1,
+    max: 3,
+    step: 0.1
+  }
+};
