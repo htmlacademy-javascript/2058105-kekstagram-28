9a99f383b117717b103ebb98c7c06386b956c2c9
Anastasia Mitrofanova <mitrofanovaa70@gmail.com>
2023-04-02 23:21:08 +0300

    Фото-эффекты

--- js/photo-effects.js
+++ js/photo-effects.js
@@ -30,3 +30,56 @@ const PHOTO_EFFECTS = {
     step: 0.1
   }
 };
+
+const UNIT = {
+  invert: '%',
+  blur: 'px'
+};
+
+const image = document.querySelector('.img-upload__preview img');
+const effectLevel = document.querySelector('.effect-level__value');
+const slider = document.querySelector('.img-upload__effect-level');
+
+const createSlider = () => {
+  noUiSlider.create(slider, {
+    range: {
+      min: 0,
+      max: 100
+    },
+    start: 100,
+    step: 1,
+    connect: 'lower'
+  });
+};
+
+const resetEffect = () => {
+  image.style.filter = null;
+  slider.classList.add('hidden');
+};
+
+const addEffect = (evt) => {
+  if(evt.target.value === 'none') {
+    resetEffect();
+    return;
+  }
+  slider.classList.remove('hidden');
+
+  const {name, min, max, step} = PHOTO_EFFECTS[evt.target.value];
+  const unit = UNIT[name] ? UNIT[name] : '';
+
+  slider.noUiSlider.updateOptions({
+    range: {
+      min,
+      max
+    },
+    start: max,
+    step,
+    connect: 'lower'
+  });
+  slider.noUiSlider.on('update', () => {
+    effectLevel.value = slider.noUiSlider.get();
+    image.style.filter = `${name}(${effectLevel.value}${unit})`;
+  });
+};
+
+export{addEffect, resetEffect, createSlider};
